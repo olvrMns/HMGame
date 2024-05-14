5544ee780bc8485df62036380259b0183220e89b
olvrMns <oliviermansuy7@gmail.com>
2024-05-14 11:32:19 -0400

    Webpack config

---
+++ inst.md
@@ -0,0 +1,6 @@
+npm install webpack webpack-cli --save-dev
+npm i webpack-dev-server
+npm install typescript --save-dev
+npm install --save-dev html-webpack-plugin
+npm install copy-webpack-plugin --save-dev
+npm install --save-dev typescript ts-loader
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+
+module.exports = (env, argv) => {
+    return({
+        entry: "./src/app.ts",
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: "out.js"
+        },
+        devServer: {
+            //https://betterexplained.com/articles/how-to-optimize-your-site-with-gzip-compression/
+            compress: true,
+            port: 5556,
+            host: "127.0.0.1"
+        },
+        performance: {hints: false},
+        mode: 'development',
+        resolve: {
+            extensions: [".ts", ".js"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'public/index.html',
+                minify: false
+            }),
+
+            new CopyPlugin({
+                patterns: [{ from: "songs/"}]
+            })
+        ]
+    })
+}
